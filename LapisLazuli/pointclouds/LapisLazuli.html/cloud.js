72cb34fb2cec231b80323931087e5d7b0593a978
Tanguy <tangracine@gmail.com>
2023-06-15 16:19:01 +0200

    add LapisLazuli

---
+++ LapisLazuli/pointclouds/LapisLazuli.html/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 9999863,
+    "boundingBox": {
+        "lx": -216811.30584716798,
+        "ly": 872806.2141113281,
+        "lz": 13025.36742401123,
+        "ux": -216727.84368896485,
+        "uy": 872889.6762695313,
+        "uz": 13108.829582214355
+    },
+    "tightBoundingBox": {
+        "lx": -216811.305847,
+        "ly": 872806.214111,
+        "lz": 13025.367424,
+        "ux": -216771.467651,
+        "uy": 872889.67627,
+        "uz": 13033.838917
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.7228034734725952,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

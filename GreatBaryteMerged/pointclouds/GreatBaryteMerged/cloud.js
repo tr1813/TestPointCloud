0fea82adb27e2b36b5a2869e61a6b15f9a66572a
Tanguy <tangracine@gmail.com>
2023-06-19 19:31:23 +0200

    Great Baryte second half

---
+++ GreatBaryteMerged/pointclouds/GreatBaryteMerged/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 18363252,
+    "boundingBox": {
+        "lx": -84.74308013916016,
+        "ly": -77.10533142089844,
+        "lz": 4.502915859222412,
+        "ux": 196.68104553222657,
+        "uy": 204.31879425048829,
+        "uz": 285.92704153060915
+    },
+    "tightBoundingBox": {
+        "lx": -84.74307999999999,
+        "ly": -77.10533099999999,
+        "lz": 4.502916,
+        "ux": 196.68104599999999,
+        "uy": 188.388016,
+        "uz": 136.19662499999999
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "CANUPO.confidence",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "CANUPO.class",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        }
+    ],
+    "spacing": 2.437204360961914,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

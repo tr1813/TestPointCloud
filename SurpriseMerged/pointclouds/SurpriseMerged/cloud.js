da25fccb04059c7eb6bf311f86aad953501236d3
Tanguy <tangracine@gmail.com>
2023-06-19 18:34:20 +0200

    new point cloud

---
+++ SurpriseMerged/pointclouds/SurpriseMerged/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 15552673,
+    "boundingBox": {
+        "lx": 806458.236328125,
+        "ly": 4473586.210330009,
+        "lz": 1086.702880859375,
+        "ux": 806533.1635742188,
+        "uy": 4473661.137576103,
+        "uz": 1161.630126953125
+    },
+    "tightBoundingBox": {
+        "lx": 806458.23633,
+        "ly": 4473586.21033,
+        "lz": 1086.7028800000003,
+        "ux": 806489.37891,
+        "uy": 4473653.46817,
+        "uz": 1161.63013
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "Original__cloud__index",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "Coord.__Z",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        }
+    ],
+    "spacing": 0.6488890051841736,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

df5b41d89ac83b4aa5bb5abf22eaf8a76b8eb3eb
Tanguy <tangracine@gmail.com>
2023-06-21 10:41:39 +0200

    Fersaman Merged second half

---
+++ FersmanaMerged/pointclouds/FersmanaMerged/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 15383792,
+    "boundingBox": {
+        "lx": -16.077178955078126,
+        "ly": -30.785491943359376,
+        "lz": -20.59002685546875,
+        "ux": 349.59375,
+        "uy": 334.88543701171877,
+        "uz": 345.0809020996094
+    },
+    "tightBoundingBox": {
+        "lx": -16.077179,
+        "ly": -30.785491999999999,
+        "lz": -20.590027,
+        "ux": 349.59375,
+        "uy": 207.13385,
+        "uz": 198.55514499999999
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 3.1668031215667726,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

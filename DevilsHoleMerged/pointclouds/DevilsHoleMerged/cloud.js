0835289630c1fac06186ddb03c93b481347b9acf
Tanguy <tangracine@gmail.com>
2023-06-26 17:52:02 +0200

    DevilsHoles 2nd half

---
+++ DevilsHoleMerged/pointclouds/DevilsHoleMerged/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 21965003,
+    "boundingBox": {
+        "lx": -123.66294860839844,
+        "ly": -89.02503967285156,
+        "lz": -20.43440055847168,
+        "ux": 356.6573181152344,
+        "uy": 391.29522705078127,
+        "uz": 459.88586616516116
+    },
+    "tightBoundingBox": {
+        "lx": -123.662949,
+        "ly": -89.02503999999999,
+        "lz": -20.434400999999999,
+        "ux": 170.645401,
+        "uy": 391.29522699999998,
+        "uz": 78.813614
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 4.159695625305176,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

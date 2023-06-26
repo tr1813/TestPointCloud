cc4a8a571777151d8ca9ab97109e5b32b5dbed0e
Tanguy <tangracine@gmail.com>
2023-06-26 19:17:03 +0200

    add TyuyaMuyun merged

---
+++ TyuyaMuyunMerged/pointclouds/TyuyaMuyunMerged/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 20682009,
+    "boundingBox": {
+        "lx": 804283.9885864258,
+        "ly": 4473049.5,
+        "lz": 1070.6210899353027,
+        "ux": 806672.5,
+        "uy": 4475438.011413574,
+        "uz": 3459.1325035095215
+    },
+    "tightBoundingBox": {
+        "lx": 804283.98859,
+        "ly": 4473049.5,
+        "lz": 1070.62109,
+        "ux": 806672.5,
+        "uy": 4473807.06572,
+        "uz": 1330.45557
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "Original__cloud__index",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "Coord.__Z",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        }
+    ],
+    "spacing": 20.685115814208986,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

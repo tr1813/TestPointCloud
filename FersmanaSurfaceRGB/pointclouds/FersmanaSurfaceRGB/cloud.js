bd4fa075e2b5e76f11e8f98ebf28e92f384e5f93
Tanguy <tangracine@gmail.com>
2023-06-26 14:12:32 +0200

    Fersmana Surface second half

---
+++ FersmanaSurfaceRGB/pointclouds/FersmanaSurfaceRGB/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 19996750,
+    "boundingBox": {
+        "lx": 294699.57458496096,
+        "ly": 4470131.651107788,
+        "lz": 1225.0278415679932,
+        "ux": 295066.4768066406,
+        "uy": 4470498.553329468,
+        "uz": 1591.9300632476807
+    },
+    "tightBoundingBox": {
+        "lx": 294699.57458,
+        "ly": 4470131.65111,
+        "lz": 1225.02784,
+        "ux": 295066.47681,
+        "uy": 4470408.87103,
+        "uz": 1319.09409
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 3.17746639251709,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

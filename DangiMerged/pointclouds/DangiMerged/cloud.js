6b5c1096199252135ce1babbdaee381d42191374
Tanguy <tangracine@gmail.com>
2023-06-26 19:14:47 +0200

    add Dangi Adit

---
+++ DangiMerged/pointclouds/DangiMerged/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 15035301,
+    "boundingBox": {
+        "lx": 805542.5,
+        "ly": 4473117.0,
+        "lz": 1070.2092304229736,
+        "ux": 806673.25,
+        "uy": 4474247.75,
+        "uz": 2200.9592304229736
+    },
+    "tightBoundingBox": {
+        "lx": 805542.5,
+        "ly": 4473117.0,
+        "lz": 1070.2092300000002,
+        "ux": 806673.25,
+        "uy": 4473501.0,
+        "uz": 1160.7981000000002
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 9.792582511901856,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

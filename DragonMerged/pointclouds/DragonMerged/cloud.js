37c66a669897f66348761d65d33fb5d9a2678b8a
Tanguy <tangracine@gmail.com>
2023-06-26 16:47:45 +0200

    dragon merged 2nd half

---
+++ DragonMerged/pointclouds/DragonMerged/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 15374287,
+    "boundingBox": {
+        "lx": 805364.8125,
+        "ly": 4473045.5,
+        "lz": 1179.7827072143555,
+        "ux": 805637.8125,
+        "uy": 4473318.5,
+        "uz": 1452.7827072143555
+    },
+    "tightBoundingBox": {
+        "lx": 805364.8125,
+        "ly": 4473045.5,
+        "lz": 1179.7827100000002,
+        "ux": 805615.8125,
+        "uy": 4473318.5,
+        "uz": 1326.5571300000001
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 2.3642494678497316,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

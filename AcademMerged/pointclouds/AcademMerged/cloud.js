f99b32503ce1ae2558c767c6d48e0d98f8e33736
Tanguy <tangracine@gmail.com>
2023-06-27 13:36:51 +0200

    Academ cleanup

---
+++ AcademMerged/pointclouds/AcademMerged/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 14972691,
+    "boundingBox": {
+        "lx": 804336.2858886719,
+        "ly": 4473601.176810026,
+        "lz": 1194.91943359375,
+        "ux": 804424.9908447266,
+        "uy": 4473689.881766081,
+        "uz": 1283.6243896484375
+    },
+    "tightBoundingBox": {
+        "lx": 804336.28589,
+        "ly": 4473601.17681,
+        "lz": 1194.9194300000001,
+        "ux": 804381.33472,
+        "uy": 4473688.15756,
+        "uz": 1283.6243900000002
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.7682074308395386,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

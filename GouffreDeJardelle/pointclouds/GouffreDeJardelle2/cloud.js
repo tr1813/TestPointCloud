0477873777a9b4285dc9b4c595634ac9f94a0805
Tanguy Racine <tang.mich.root@gmail.com>
2023-11-14 17:23:36 +0100

    add gouffre de  jardelle

---
+++ GouffreDeJardelle/pointclouds/GouffreDeJardelle2/cloud.js
@@ -0,0 +1,99 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 150582098,
+    "boundingBox": {
+        "lx": 897059.2290000916,
+        "ly": 2220321.160999298,
+        "lz": 721.9240036010742,
+        "ux": 897193.7389945984,
+        "uy": 2220455.670993805,
+        "uz": 856.4339981079102
+    },
+    "tightBoundingBox": {
+        "lx": 897059.229,
+        "ly": 2220321.161,
+        "lz": 721.924,
+        "ux": 897121.542,
+        "uy": 2220373.542,
+        "uz": 856.434
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "Coord.__Z",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "CANUPO.class",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "CANUPO.confidence",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        }
+    ],
+    "spacing": 1.1648907661437989,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
